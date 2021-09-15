df6e4ddf184e4cdf5199ffc76639881b02d2bee0
Marie NOTIX <marienotix@Maries-MacBook-Pro.local>
2021-09-15 14:51:21 +0200

    Allergen entity refactored to enum

--- src/main/java/cz/mavenclu/cookbook/dao/AllergenRepository.java
+++
@@ -1,9 +0,0 @@
-package cz.mavenclu.cookbook.dao;
-
-import cz.mavenclu.cookbook.entity.Allergen;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AllergenRepository extends JpaRepository<Allergen, Long> {
-}
